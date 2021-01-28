cae15eed4e6c8d87678ef7e0f9473f2725d86b82
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-01-28 12:05:03 +0100

    Logotype component

---
+++ src/components/common/Logotype/index.jsx
@@ -0,0 +1,9 @@
+import "./logotype.sass"
+
+import { Person } from '@material-ui/icons';
+
+export const Logotype = () => {
+    return (
+        <a href="#banner"><Person className="person-icon" /></a>
+    )
+}
