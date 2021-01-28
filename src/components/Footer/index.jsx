e12be18caaee77abd145c5f3fc0a39ba6d826fdb
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-01-28 12:03:34 +0100

    Footer added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,19 @@
 import "./footer.sass"
 
+import { GitHub, LinkedIn, MailOutline } from '@material-ui/icons';
+
 export const Footer = () => {
     return (
         <footer id="footer">
-            <div class="footer-inner">
-                <p>© 2020 Erix Mamani Villacresis. All rights reserved.</p>
+            <div className="footer-inner">
+                <div className="footer-credits">
+                    <p>© 2021 Erix Mamani Villacresis. All rights reserved.</p>
+                </div>
+                <div className="footer-icons">
+                    <a href="https://github.com/ErixMV" target="_BLANK" rel="noreferrer"><GitHub /></a>
+                    <a href="https://www.linkedin.com/in/erixmamani/" target="_BLANK" rel="noreferrer"><LinkedIn /></a>
+                    <a href="mailto:erixmv022@gmail.com"><MailOutline /></a>
+                </div>
             </div>
         </footer>
     )
