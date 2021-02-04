8aa7cd373fe600042b5c131c66d26dffa9dd8a5b
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-04 12:24:51 +0100

    imported new components to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import './App.sass';
-import { Banner } from './components/Banner/index';
-import { Footer } from './components/Footer/index';
+import { Banner } from './components/Banner';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Projects } from './components/Projects';
+import { Weathere } from './components/pages/Weathere';
+import { Contact } from './components/Contact/index';
+import { About } from './components/About/index';
 
 function App() {
   return (
-    <div className="App">
-      <Banner />
+    <Router>
+      <div className="App">
+        <Header />
+      </div>
+      <Switch>
+        <Route path="/weathere" component={Weathere} />
+        <Route exact path="/">
+          <Banner />
+          <Projects />
+          <About />
+          <Contact />
+        </Route>
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
