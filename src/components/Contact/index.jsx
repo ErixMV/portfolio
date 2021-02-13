8a9e2716177aa4ebb06e329ab31f0d049dc7a51e
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-13 23:59:37 +0100

    Updated contact component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,17 +1,27 @@
 import "./contact.sass"
-import { MailOutline } from '@material-ui/icons';
+import { LinkedIn, GitHub } from '@material-ui/icons';
 export const Contact = () => {
     return (
         <div id="contact" className="section">
             <div className="section-title">
                 Get in touch
             </div>
-            <div className="section-desc">
-                I'm currently looking for new opportunities on Web Development, Front-End Development and Back-End Development.
-                If you want to discuss a project, an opportunity or just say hi, my inbox will always be available!
-            </div>
-            <div className="links">
-                <button><MailOutline /></button>
+            <div className="section-body">
+                <div className="section-desc">
+                    I'm currently looking for new opportunities on Web Development, Front-End Development and Back-End Development.
+                    If you want to discuss a project, an opportunity or just say hi, my inbox will always be available!
+                
+                    <div className="link-mail">
+                        <div className="btn-under-effect"><span className="noselect"><a href="mailto:erixmv022@gmail.com">erixmv022@gmail.com</a></span><div className="circle"></div></div>
+                        {/* <a href="mailto:erixmv022@gmail.com">erixmv022@gmail.com</a> */}
+                    </div>
+                </div>
+                <div className="contact-links">
+                    <div className="links">
+                        <a href="https://www.linkedin.com/in/erixmamani/" target="_BLANK" rel="noreferrer"><LinkedIn /></a>
+                        <a href="https://github.com/ErixMV" target="_BLANK" rel="noreferrer"><GitHub /></a>
+                    </div>
+                </div>
             </div>
         </div>
     )
