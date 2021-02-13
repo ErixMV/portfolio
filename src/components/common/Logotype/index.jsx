4aab56c18ca2c2b2ce9d6901aa5dea5bce64f8dc
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-13 23:42:14 +0100

    Logotype updated

--- src/components/common/Logotype/index.jsx
+++ src/components/common/Logotype/index.jsx
@@ -1,9 +1,8 @@
 import "./logotype.sass"
-
-import { Person } from '@material-ui/icons';
+import { PlayArrowOutlined } from '@material-ui/icons';
 
 export const Logotype = () => {
     return (
-        <a href="#banner"><Person className="person-icon" /></a>
+        <a href="/#banner" className="to-top"><PlayArrowOutlined className="person-icon"/></a>
     )
 }
