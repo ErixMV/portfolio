f71855acf0153447aee39224fec7b0013d070c70
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-13 23:48:11 +0100

    Updated project

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,21 +1,21 @@
 import { GitHub, Language, AddCircleOutline } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
-import "aos/dist/aos.css";
+// import "aos/dist/aos.css";
 
 import { useEffect } from 'react';
-import AOS from "aos";
 
-export const Project = ({ title, desc, img, i, github, web }) => {
+export const Project = ({ title, desc, img, url, svg, i, github, web }) => {
 
     useEffect(() => {
-        AOS.init({
-            duration: 1000
-        })
+        // AOS.init({
+        //     duration: 1000
+        // })
     }, [])
 
     return (
         <figure className="project" data-folio={i} data-link="WheThere" data-aos="fade-up" >
-            <img src={`/static/${img}`} alt="1" />
+
+            <img src={`/static/${img ? img : svg}`} style={img ? {} : { height: "inherit", padding: "3rem", background: "radial-gradient(#333, black)" }} alt="1" />
             <figcaption className="project__caption">
                 <div className="project-title">
                     {title}
@@ -26,8 +26,8 @@ export const Project = ({ title, desc, img, i, github, web }) => {
                     </div>
                     <div className="project-buttons">
                         <a href={github} target="_BLANK" rel="noreferrer"><GitHub /></a>
-                        <a href={web} target="_BLANK" rel="noreferrer"><Language /></a>
-                        <Link to="/weathere"><AddCircleOutline /></Link>
+                        {web && <a href={web} target="_BLANK" rel="noreferrer"><Language /></a>}
+                        <Link to={`/${url}`}><AddCircleOutline /></Link>
                     </div>
                 </div>
             </figcaption>
