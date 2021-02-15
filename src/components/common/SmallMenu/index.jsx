ebbc72a831eabcf1e26a994f5e53de99874b0954
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-02-15 11:30:05 +0100

    toggle navbar hook

--- src/components/common/SmallMenu/index.jsx
+++ src/components/common/SmallMenu/index.jsx
@@ -1,15 +1,26 @@
 import "./bm-burger.sass"
+import { useState } from "react"
 
 import { slide as Menu } from 'react-burger-menu'
 
 export const SmallMenu = () => {
 
+    const [open, isOpen] = useState(false);
+
+    const openHandler = () => {
+        isOpen(false)
+    }
+
+    const openMenu = () => {
+        isOpen(true)
+    }
+
     return (
-        <Menu disableAutoFocus right>
-            <a id="home" className="menu-item" href="/#banner">Home</a>
-            <a id="Projects" className="menu-item" href="/#e-projects">Projects</a>
-            <a id="About" className="menu-item" href="/#e-about">About</a>
-            <a id="contact" className="menu-item" href="/#e-contact">Contact</a>
+        <Menu disableAutoFocus right onOpen={openMenu} onClose={openHandler} isOpen={open}>
+            <a onClick={openHandler} id="home" className="menu-item" href="/#banner">Home</a>
+            <a onClick={openHandler} id="Projects" className="menu-item" href="/#e-projects">Projects</a>
+            <a onClick={openHandler} id="About" className="menu-item" href="/#e-about">About</a>
+            <a onClick={openHandler} id="contact" className="menu-item" href="/#e-contact">Contact</a>
         </Menu>
     )
 }
